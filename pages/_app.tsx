769483bbf1e7cf13a7a1e753c6a54d1510a6db12
Jhoandris B <andybaronp@gmail.com>
2022-04-17 15:53:57 -0500

    add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,45 @@ import React, { FC } from 'react'
 
 import { AppProps } from 'next/app'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import {
+  Box,
+  ChakraProvider,
+  Container,
+  Divider,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import theme from '../theme'
 
 import '../styles/globals.css'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+        <Container
+          borderRadius='sm'
+          backgroundColor='white'
+          boxShadow='md'
+          marginY={4}
+          maxWidth={'container.xl'}
+          padding={4}
+        >
+          <VStack marginBottom={6}>
+            <Image
+              src='https://picsum.photos/150/150'
+              alt='imag'
+              borderRadius={9999}
+            />
+            <Heading>Tienda xxxxx</Heading>
+            <Text>El almacen xxxx</Text>
+          </VStack>
+          <Divider />
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </ChakraProvider>
   )
 }
