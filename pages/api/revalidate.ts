00fbfb8150bda934420b38a5794c22a2821a1259
Jhoandris B <andybaronp@gmail.com>
2022-04-19 07:59:24 -0500

    fixed

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,9 +4,10 @@ export default async function handler(
   req: NextApiRequest,
   response: NextApiResponse
 ) {
-  console.log(req.headers['x-secret'])
   if (req.headers['x-secret'] === process.env.REVALIDATE) {
+    console.log('validate')
     await response.unstable_revalidate('/')
+
     return response.json({ revalidate: true })
   }
   return response.status(401).end()
